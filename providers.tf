976897b1f6409f15d00089170ef3efe2b804bf46
mohamedash7 <82774508+mohamedash7@users.noreply.github.com>
2022-04-24 22:19:54 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-access_key = "AKIA3J2NA7RDJPHYBVOO" 
-secret_key = "fgwL+C6fKUEECClr9ddKDIhrVdBPVPsjKhfgOKuE"
+access_key = "your access key" 
+secret_key = "your secret key"
 region = "us-east-1"
 }
 
